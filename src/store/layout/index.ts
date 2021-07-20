2516e85c3fc168a160190c0d85cf9cd85b8de7f0
João Cruz <jcruzaraujoneto@outlook.com>
2021-07-19 23:49:36 -0400

    updated layout store

--- src/store/layout/index.ts
+++ src/store/layout/index.ts
@@ -8,10 +8,20 @@ const slice = createSlice({
   reducers: {
     setLoadingFn (state, action: PayloadAction<boolean>) {
       state.loading = action.payload
+    },
+    showMessageFn (state, action: PayloadAction<{ text: string, duration?: number }>) {
+      state.message.show = true
+      state.message.text = action.payload.text
+      state.message.duration = action.payload.duration || null
+    },
+    hideMessageFn (state) {
+      state.message.show = false
+      state.message.text = null
+      state.message.duration = null
     }
   }
 })
 
 export default slice.reducer
 
-export const { setLoadingFn } = slice.actions
+export const { setLoadingFn, showMessageFn, hideMessageFn } = slice.actions
--- src/store/layout/state.ts
+++ src/store/layout/state.ts
@@ -1,9 +1,19 @@
 interface LayoutState {
-  loading: boolean
+  loading: boolean,
+  message: {
+    show: boolean,
+    text: string | null,
+    duration: number | null
+  }
 }
 
 const state: LayoutState = {
-  loading: false
+  loading: false,
+  message: {
+    show: false,
+    text: null,
+    duration: null
+  }
 }
 
 export default state
