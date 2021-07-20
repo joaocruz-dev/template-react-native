fe7545506c32dff6363edbb7177ccc1f202ca7cb
João Cruz <jcruzaraujoneto@outlook.com>
2021-07-19 23:51:42 -0400

    updated Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,21 +6,21 @@ import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
 import theme from '@/services/theme'
 
 interface ButtonProps extends RectButtonProps {
-  title: string,
+  label: string,
   color?: string,
   textColor?: string
 }
 
 export default class Button extends React.Component<ButtonProps> {
   static propTypes = {
+    label: PropTypes.string.isRequired,
     color: PropTypes.string,
-    textColor: PropTypes.string,
-    title: PropTypes.string.isRequired
+    textColor: PropTypes.string
   }
 
   static defaultProps = {
-    textColor: 'white',
-    color: theme.colors.primary
+    color: theme.colors.primary,
+    textColor: 'white'
   }
 
   render () {
@@ -28,10 +28,10 @@ export default class Button extends React.Component<ButtonProps> {
       <View style={styles.container}>
         <RectButton
           {...this.props}
-          style={[styles.button, { backgroundColor: this.props.color }, this.props.style || {}]}
+          style={[styles.button, { backgroundColor: this.props.color }, this.props.style]}
         >
-          <Text style={[styles.title, { color: this.props.textColor }]}>
-            {this.props.title}
+          <Text style={[styles.label, { color: this.props.textColor }]}>
+            {this.props.label}
           </Text>
         </RectButton>
       </View>
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row'
   },
-  title: {
+  label: {
     flex: 1,
     fontSize: 16,
     textAlign: 'center'
